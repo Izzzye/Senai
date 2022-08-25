6e4e7ef99fcf0caaec4aad353a62cfb531b514dc
Isabella Costa <111472284+Izzzye@users.noreply.github.com>
2022-08-25 11:25:08 -0300

    Update ex03.c

--- 1des/FPOO/aula05/exercicios/lista_03_ead/ex03.c
+++ 1des/FPOO/aula05/exercicios/lista_03_ead/ex03.c
@@ -1,8 +1,21 @@
-/*FaÁa um programa para uma loja de tintas. O programa dever· pedir o tamanho em mentros quadrados
-da ·rea a ser pintada. Considere que a cobertura da tinta È 1 litro para cada 6 metros quadrados e 
-que a tinta È vendida em latas de 18 litros, que custam R$80,00 ou em galıes de 3,6 litros, que 
-custam R$25,00. Informe ao usu·rio as quantidades de tinta a serem compradas e os respectivos preÁos
-em 2 situaÁıes: 
+/*Fa√ßa um programa para uma loja de tintas. O programa dever√° pedir o tamanho em mentros quadrados
+da √°rea a ser pintada. Considere que a cobertura da tinta √© 1 litro para cada 6 metros quadrados e 
+que a tinta √© vendida em latas de 18 litros, que custam R$80,00 ou em gal√µes de 3,6 litros, que 
+custam R$25,00. Informe ao usu√°rio as quantidades de tinta a serem compradas e os respectivos pre√ßos
+em 2 situa√ß√µes: 
 comprar apenas latas de 18 litros;
-comprar apenas galıes de 3,6 litros.
+comprar apenas gal√µes de 3,6 litros.
 */
+
+#include <stdio.h>
+#include <locale.h>
+
+int main (){
+	setlocale(LC_ALL, "");
+	float area;
+	
+	printf("Informe o tamanho em metros quadrados da √°rea a ser pintada: ");
+	scanf("%d", &area);
+	
+	
+}
