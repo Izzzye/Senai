ae9a0c7afc1c98a6981903caa34f83613575d11c
Isabella Costa <111472284+Izzzye@users.noreply.github.com>
2022-08-25 11:23:16 -0300

    Update ex03.c

--- 1des/FPOO/aula06/exercicios/ex03.c
+++ 1des/FPOO/aula06/exercicios/ex03.c
@@ -1,1 +1,19 @@
 /*Utilizando apenas um vetor de tamanho 6, troque os valores de maneira inversa;*/
+
+#include <stdio.h>
+#include <locale.h>
+
+int main (){
+	setlocale(LC_ALL,"");
+	
+	int vetor[6], aux[6];
+	int i, cont = 1;
+	
+	
+	printf("Digite os valores do  vetor:\n");
+	for (i = 0; i < 6; i++){
+		printf("%dº valor: ", cont);
+		scanf ("%d", &vetor[i]);
+		cont++;
+	}	
+}
