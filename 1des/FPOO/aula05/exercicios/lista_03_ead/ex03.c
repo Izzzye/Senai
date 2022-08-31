512d8232a8bd059e336d5f9bc0919f903b1a942b
Isabella Costa <111472284+Izzzye@users.noreply.github.com>
2022-08-31 10:55:21 -0300

    Update ex03.c

--- 1des/FPOO/aula05/exercicios/lista_03_ead/ex03.c
+++ 1des/FPOO/aula05/exercicios/lista_03_ead/ex03.c
@@ -9,13 +9,26 @@ comprar apenas galões de 3,6 litros.
 
 #include <stdio.h>
 #include <locale.h>
+#include <math.h>
 
 int main (){
 	setlocale(LC_ALL, "");
-	float area;
+	float area, tinta, lata, galao, precol, precog, a, b;
 	
-	printf("Informe o tamanho em metros quadrados da área a ser pintada: ");
-	scanf("%d", &area);
+	printf("Informe o tamanho em m² da área a ser pintada: ");
+	scanf("%f", &area);
+		
+	tinta = area / 6.0;
+	
+	lata = tinta / 18.0;
+	precol = lata * 80.0;
+	
+	galao = tinta / 3.6;
+	precog = galao * 25.0;
 	
 	
+	
+	printf ("\nÉ necessário %.2f litros de tinta para pintar a área de %.0fm²\n", tinta, area);
+	printf ("Comprando apenas latas de 18L serão necessárias %.0f.Com valor de R$%.2f.\n", ceil(lata), precol);
+	printf ("Comprando apenas galões de 3,6L serão necessárias %.0f. Com valor de R$%.2f.\n", ceil(galao), precog);
 }
