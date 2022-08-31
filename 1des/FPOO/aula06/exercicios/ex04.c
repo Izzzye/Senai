f523710eebed22fc6994b1a0ac0862a2d0634d58
Isabella Costa <111472284+Izzzye@users.noreply.github.com>
2022-08-31 08:53:25 -0300

    Update ex04.c

--- 1des/FPOO/aula06/exercicios/ex04.c
+++ 1des/FPOO/aula06/exercicios/ex04.c
@@ -6,7 +6,7 @@
 int main(){
 	setlocale(LC_ALL,"");
 	int vetor[10];
-	int i, cont = 1, val = 0, flag = 0;
+	int i = 0, cont = 1, val = 0;
 	
 	printf("Insira os números do vetor:\n");
 	for (i = 1; i <= 10; i++){
@@ -17,10 +17,7 @@ int main(){
 	
 	printf("Qual valor deseja localizar? ");
 	scanf("%d", &val);
+	printf("%d", vetor[val]);
 	
-	for(i = 1; i <=10; i++){
-		if (val == vetor[i]){
-			printf("O valor está na posição %d\n", i);
-		}
-	}	
+	
 }
