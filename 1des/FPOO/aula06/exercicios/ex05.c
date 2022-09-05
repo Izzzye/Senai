e896b738a1307abe16a89b1f660b3e7506785821
Isabella Costa <111472284+Izzzye@users.noreply.github.com>
2022-09-05 08:39:58 -0300

    Update ex05.c

--- 1des/FPOO/aula06/exercicios/ex05.c
+++ 1des/FPOO/aula06/exercicios/ex05.c
@@ -1,21 +1,34 @@
-//Faça um programa que leia 10 valores inteiros em qualquer ordem e os classifique em ordem crescente.
+//FaÃ§a um programa que leia 10 valores inteiros em qualquer ordem e os classifique em ordem crescente.
 
 #include <stdio.h>
 #include <locale.h>
 int main (){
 	setlocale (LC_ALL,"");
-	int vetor[10], i = 0;
-	int cont = 1;
+	int vetor[10]; // vetor a ser classificado
+	int i = 0, j; // contadores
+	int aux; // auxiliar para fazer trocas
 	
 	printf("Digite os valores do  vetor:\n");
 	for (i = 0; i < 10; i++){
-		printf("%dº valor: ", cont);
-		scanf ("%d", &vetor[i]);
-		cont++;
+		printf("%dÂº valor: ", i + 1);
+		scanf ("%d", &vetor[i]);		
 	}
 	
-	
-	
-	printf("Esses são os valores em ordem crescente:");
-}
+// Processamento - Algoritimo de classificaÃ§Ã£o e troca
+	for ( i = 0; i < 10; i++){
+		for (j = i + 1; j < 10; j++){
+			if (vetor[i] > vetor[j]){
+				aux = vetor[i];
+				vetor[i] = vetor[j];
+				vetor[j] = aux;
+			}
+		}
+	}
 
+// Saida com o vetor ordenado
+	printf("\nValores em ordem Crescente:\n");
+	for(i = 0; i < 10; i++){
+		printf("%dÂº valor: %d\n", i + 1, vetor[i]);
+		
+	}
+}
