20a3844cdb89d9adc047d2038d88d1ad9c71fc53
Isabella Costa <111472284+Izzzye@users.noreply.github.com>
2022-10-11 08:05:18 -0300

    Add files via upload

---
+++ 1des/FPOO/Java/aula03/EstruturaEscaner/src/padrao/Main.java
@@ -0,0 +1,26 @@
+package padrao;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String nome;
+		int idade;
+		float peso;
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Digite seu nome: ");
+		nome = scan.next();
+		
+		System.out.println("Digite sua idade: ");
+		idade = scan.nextInt();
+		
+		System.out.println("Digite seu peso: ");
+		peso = scan.nextFloat();
+		
+		System.out.printf("\nNome: %s\nIdade: %d\nPeso: %.1f", nome, idade, peso);
+		
+	}
+	
+}
