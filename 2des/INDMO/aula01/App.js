dc3dc6da989dff70206c1dc211c5c5046d5cd76a
Izzzye <isabella.costa8576@gmail.com>
2023-03-28 11:23:50 -0300

    Indmo

--- 2des/INDMO/aula01/App.js
+++ 2des/INDMO/aula01/App.js
@@ -1,15 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+
 import { StyleSheet, Text, View, TextInput, Button, TouchableOpacity } from 'react-native';
 
+import ButtonCadastrar from './src/components/button-cadastrar';
+
 export default function App() {
+  const [numero, setNumero] = new useState(0);
+  const [nome, setNome] = new useState('');
+  const [vali, setVali] = new useState(0);
+  const [valii, setValii] = new useState(0);
+  const [resultado, setResultado] = new useState(0);
+
+  const texto = 'Meu Número é: ';
+
+  const incrementar = () =>{
+    let valor = numero + 1;
+    setNumero(valor);
+  }
+
+  const cadastrar = () => {
+    console.log(nome);
+    setNome('')
+  }
+
+  const somar = () => {
+    setResultado(vali + valii);
+    setVali(0);
+    setValii(0);
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Meu Primeiro App</Text>
-      <TextInput style={styles.input}/>
-      <Button title='Cadastrar'/>
-      <TouchableOpacity>
-        <Text>Autenticar</Text>
+      <Text style={styles.title}>{texto + numero}</Text>
+      <TextInput style={styles.input} onChangeText={ (val) => setNome(val) } value={nome}/>
+      <Button title='Cadastrar' onPress={ () => cadastrar()}/>
+      <TouchableOpacity style={styles.button} onPress={ () => incrementar() }>
+        <Text style={styles.textButton}>Autenticar</Text>
       </TouchableOpacity>
+      <ButtonCadastrar/>
+
+      <TextInput style={styles.input} value={vali} onChangeText={ (val) => setVali(Number(val)) }/>
+      <TextInput style={styles.input} value={valii} onChangeText={ (val) => setValii(Number(val)) }/>
+      <Button title='somar'onPress={() => somar()}/>
+      <Text style={styles.title}>O resultado da soma é: {resultado} </Text>
     </View>
   );
 }
@@ -32,5 +65,15 @@ const styles = StyleSheet.create({
     borderBottomColor: '#333',
     backgroundColor: '#e5e5e5',
     padding: '5px'
+  },
+  button: {
+    padding: '15px',
+    backgroundColor: '#df5800',
+    borderRadius: '7px'
+  },
+  textButton: {
+    color: '#333',
+    fontWeight:'bold',
+    fontSize: '1.5rem'
   }
 });
